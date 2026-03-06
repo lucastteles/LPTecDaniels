e9c0f69496d5d28f604a7739165723b10af76505
lucastteles <98498654+lucastteles@users.noreply.github.com>
2026-03-05 22:34:41 -0300

    atualizando link e adicionando novo layout ajustando tag google

--- assets/js/pages/index/tracking.js
+++ assets/js/pages/index/tracking.js
@@ -1,40 +1,10 @@
-// window.dataLayer = window.dataLayer || [];
-
-//     document.addEventListener('click', function(e) {
-
-//       const link = e.target.closest('a');
-//       if (!link) return;
-
-//       if (link.href.includes('api.whatsapp.com') || 
-//           link.href.includes('wa.me')) {
-
-//         dataLayer.push({
-//           event: 'whatsapp_click',
-//           page_location: window.location.href,
-//           page_path: window.location.pathname,
-//           link_url: link.href,
-//           link_text: link.innerText.trim()
-//         });
-
-//       }
-
-//     });
-
-  window.dataLayer = window.dataLayer || [];
-
-  document.querySelectorAll('.btn-whatsapp').forEach(button => {
-    button.addEventListener('click', function(e) {
-
-      e.preventDefault(); // 🔥 impede o <a> de abrir sozinho
-
-      dataLayer.push({ 
-        event: 'whatsapp_click' 
+ document.querySelectorAll('.btn-whatsapp').forEach(button => {
+      button.addEventListener('click', function () {
+        dataLayer.push({ 'event': 'whatsapp_click' });
+        window.open(
+          'https://api.whatsapp.com/send?phone=5521986171050&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20visita%20t%C3%A9cnica',
+          '_blank'
+        );
       });
-
-      window.open(
-        'https://api.whatsapp.com/send?phone=5521986171050&text=Gostaria+de+or%C3%A7amento+para+visita+t%C3%A9cnica',
-        '_blank'
-      );
-
     });
-  });
+    
