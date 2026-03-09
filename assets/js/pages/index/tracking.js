852840231d915d43a635b4af5284112c303f0162
lucastteles <98498654+lucastteles@users.noreply.github.com>
2026-03-09 20:10:01 -0300

    Ajustando Tag dos produtos e voltando versão da pag manutencao

--- assets/js/pages/index/tracking.js
+++ assets/js/pages/index/tracking.js
@@ -1,10 +1,18 @@
- document.querySelectorAll('.btn-whatsapp').forEach(button => {
-      button.addEventListener('click', function () {
-        dataLayer.push({ 'event': 'whatsapp_click' });
-        window.open(
-          'https://api.whatsapp.com/send?phone=5521986171050&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20visita%20t%C3%A9cnica',
-          '_blank'
-        );
-      });
-    });
-    
+// Botão de orçamento
+document.querySelectorAll('.btn-whatsapp').forEach(button => {
+  button.addEventListener('click', function () {
+    dataLayer.push({ 'event': 'whatsapp_click' });
+
+    window.open(
+      'https://api.whatsapp.com/send?phone=5521986171050&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento%20para%20visita%20t%C3%A9cnica',
+      '_blank'
+    );
+  });
+});
+
+// Botões de produto 
+document.querySelectorAll('.btn-whatsapp-produto').forEach(button => {
+  button.addEventListener('click', function () {
+    dataLayer.push({ 'event': 'whatsapp_click' });
+  });
+});
